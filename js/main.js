6b1997fe42a0653aafda93e3bbb01bbcdfad2bd7
MiguelGarcia95 <mgarcia95951@gmail.com>
2019-08-25 16:12:33 -0500

    animate background

--- js/main.js
+++ js/main.js
@@ -1,8 +1,12 @@
 // var backgroundAnim = new TimelineMax({repeat: -1});
 var backgroundAnim = new TimelineMax();
+// var loadBackgroundAnim = new TimelineMax();
 
 var sun = document.querySelector('.sun');
+// var sun = document.querySelectorAll('.tree');
 
 backgroundAnim
-  .from(sun, 2, {y: '-100px'})
-  // .to(sun, 2, {y: '-100px'})
+  .to(sun, 2, {y: '+=100px'})
+  .to(sun, 2, {y: '-=100px'})
+  .to('.tree_02', 2, {x: '+=100px', y: '+=100px'}, '-=2')
+  .to('.tree_02', 2, {x: '-=100px', y: '-=100px'})
